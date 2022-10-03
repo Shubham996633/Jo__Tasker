86c5e04e5e82a0e7f3efe4793dcc32377fe6e209
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-03 06:05:39 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-// 33:25
+const spanDate = document.querySelector('#date')
+const spanMonth = document.querySelector('#month')
+const spanYear = document.querySelector('#year')
+const spanWeekDay = document.querySelector('#weekday')
+
+
+window.addEventListener('load', () => {
+    const date = new Date()
+    const month = date.toLocaleDateString('default', {month: 'long'})
+    const year = date.getFullYear()
+    const weekday = date.toLocaleDateString('default', {weekday: 'long'})
+    const myDate = date.getDate()
+
+    spanDate.innerText = myDate
+    spanMonth.innerText = month
+    spanYear.innerText = year
+    spanWeekDay.innerText = weekday
+})
