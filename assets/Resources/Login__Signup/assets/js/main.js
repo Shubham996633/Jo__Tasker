1307a9eeab24c9e25f6abd3e13fefb27c67f8fb0
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-03 10:17:14 +0530

    update

--- assets/Resources/Login__Signup/assets/js/main.js
+++ assets/Resources/Login__Signup/assets/js/main.js
@@ -3,7 +3,6 @@ const signUp = document.getElementById('sign-up'),
       loginIn = document.getElementById('login-in'),
       loginUp = document.getElementById('login-up')
 
-
 signUp.addEventListener('click', ()=> {
     loginIn.classList.remove('block')
     loginUp.classList.remove('none')
@@ -12,11 +11,105 @@ signUp.addEventListener('click', ()=> {
     loginUp.classList.add('block')
 })
 
-
 signIn.addEventListener('click', ()=> {
     loginIn.classList.remove('none')
     loginUp.classList.remove('block')
 
     loginIn.classList.add('block')
     loginUp.classList.add('none')
+})
+
+const signUpForm = document.querySelector('.signup')
+signUpForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const name = signUpForm['name'].value
+    const email = signUpForm['email'].value
+    const password = signUpForm['password'].value
+    console.log(name, email, password)
+    signUpForm.reset()
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            Name: name,
+            Email: email,
+            Password: password
+        }).then(() => {
+            console.log('User Data Created')
+            loginIn.classList.remove('none')
+            loginUp.classList.remove('block')
+
+            loginIn.classList.add('block')
+            loginUp.classList.add('none')
+            
+        }).catch(err => {
+            console.log(err.message)
+            console.log('Fail To Create User')
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 4500,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              Toast.fire({
+                icon: 'info',
+                title: `${err.message}`
+              })
+        })
+    }).catch(err => {
+        console.log(err.message)
+        console.log('Fail To Firebase')
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'info',
+            title: `${err.message}`
+          })
+
+    })
+})
+
+const loginForm = document.querySelector('.signin')
+loginForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const loginEmail = loginForm['loginEmail'].value
+    const loginPassword = loginForm['loginPassword'].value
+    console.log(loginEmail, loginPassword)
+    auth.signInWithEmailAndPassword(loginEmail, loginPassword).then(() => {
+        console.log("User Login Successfull")
+        // location = `../../../index.html`
+    }).catch(err => {
+        console.log(err.message)
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'info',
+            title: `${err.message}`
+          })
+        console.log('User Login Fail')
+    })
 })
